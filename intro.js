1da19cd2ce21f090ce1539c93f531c1676d1f40a
Metkel <MetkelK@gmail.com>
2020-11-21 00:02:20 -0500

    added section headings

--- intro.js
+++ intro.js
@@ -1,21 +1,62 @@
 
-
 gsap.to(".name", {
-  xPercent: -100,
+  xPercent: 500,
   // rotate: '90deg',
-  // scale: 2,
+  scale: 4,
+  ease: Circ.easeInOut,
   // backgroundPosition: '1300px 0',
   scrollTrigger: {
     trigger: "body",
     start: 'top top',
-    end: '+=80%',
+    end: '+=100%',
     scrub: true,
   }
 });
 
-gsap.from('.last', {
-    xPercent: 90,
-    // scale: 0.2,
+const projectTL = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section3",
+        start: 'top center',
+        end: '+=50%',
+        scrub: true,
+        // markers: true,
+    }
+})
+projectTL.to('.name', {
+    xPercent: -10,
+    rotate: '-90deg',
+    scale: 2,
+    zIndex: 1,
+    ease: Circ.easeInOut,
+})
+
+gsap.to('.last', {
+    xPercent: -500,
+    scale: 4,
+    ease: Circ.easeInOut,
+    scrollTrigger: {
+        trigger:'body',
+        start: 'top top',
+        end: '+=90%',
+        scrub: true,
+    }
+})
+
+gsap.from('.skillsName', {
+    xPercent: -500,
+    scale: 4,
+    ease: Circ.easeInOut,
+    scrollTrigger: {
+        trigger:'.section1',
+        start: 'bottom bottom',
+        end: '+=20%',
+        scrub: true,
+    }
+})
+
+gsap.to('nav', {
+    background: 'linear-gradient(360deg,#000000,#152331)',
+    ease: Circ.easeInOut,
     scrollTrigger: {
         trigger:'body',
         start: 'top top',
@@ -26,13 +67,13 @@ gsap.from('.last', {
 
 
 const tl = gsap.timeline({paused: true})
-tl.to('#wave', 0.3, {
+tl.to('#wave', 0.5, {
     attr: {
         fill: '#000',
         stroke: '#fff'
     },
     scale: 0.95,
-
+    ease: Circ.easeInOut,
 })
 
 document.querySelector('.logo').addEventListener('mouseenter', () => {
