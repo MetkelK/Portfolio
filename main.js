4630f521ea7c78775d1e5df3c9e4eba90b9ba1d9
Metkel K <metkelk@gmail.com>
2021-04-16 16:26:23 -0400

    fixed broken tags

--- main.js
+++ main.js
@@ -1,85 +1,79 @@
 //LOGO ANIMATION
-let logoTL = gsap.timeline({ paused: true })
+let logoTL = gsap.timeline({ paused: true });
 
-logoTL.to('#logo', 0.3, {
-	fill: '#000',
-	stroke: '#fff',
-	scale: 0.99
-})
+logoTL.to("#logo", 0.3, {
+  fill: "#000",
+  stroke: "#fff",
+  scale: 0.99,
+});
 
-document.getElementById('logo').addEventListener('mouseenter', () => {
-	logoTL.play()
-})
+document.getElementById("logo").addEventListener("mouseenter", () => {
+  logoTL.play();
+});
 
-document.getElementById('logo').addEventListener('mouseleave', () => {
-	logoTL.reverse()
-})
-
+document.getElementById("logo").addEventListener("mouseleave", () => {
+  logoTL.reverse();
+});
 
 //MOBILE MENU FUNCTIONS
-const openMenu = document.getElementById('openMenu');
-const closeMenu = document.getElementById('closeMenu');
-const mobileMenu = document.getElementById('mobileMenu');
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
 
 function open() {
-	openMenu.style.display = 'none';
-	closeMenu.style.display = 'block';
-	gsap.to(mobileMenu, 0.3, {
-		display: 'flex',
-		opacity: 0.9,
-		ease: 'expo.inOut',
-	})
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+  gsap.to(mobileMenu, 0.3, {
+    display: "flex",
+    opacity: 0.9,
+    ease: "expo.inOut",
+  });
 }
 
-openMenu.addEventListener('click', open);
+openMenu.addEventListener("click", open);
 
 function close() {
-	openMenu.style.display = 'block';
-	closeMenu.style.display = 'none';
-	gsap.to(mobileMenu, 0.3, {
-		display: 'none',
-		opacity: 0,
-		ease: 'expo.inOut',
-	})
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+  gsap.to(mobileMenu, 0.3, {
+    display: "none",
+    opacity: 0,
+    ease: "expo.inOut",
+  });
 }
 
-closeMenu.addEventListener('click', close);
+closeMenu.addEventListener("click", close);
 
-document.querySelectorAll('#mobileMenu a').forEach(link => {
-	link.addEventListener('click', close)
-})
+document.querySelectorAll("#mobileMenu a").forEach((link) => {
+  link.addEventListener("click", close);
+});
 
 //NAV SCROLL TO
-gsap.utils.toArray('nav a').forEach(function(link) {
-	link.addEventListener('click', function(e) {
-		e.preventDefault();
-		gsap.to(window, {
-			duration: 0.5,
-			scrollTo: e.target.getAttribute('href')
-		});
-	});
+gsap.utils.toArray("nav a").forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(link);
+    gsap.to(window, {
+      duration: 0.5,
+      scrollTo: e.target.getAttribute("href"),
+    });
+  });
 });
 
-
-//ARROW SCROLL 
-document.getElementById('scroll').addEventListener('click', function(e) {
-	e.preventDefault();
-	gsap.to(window, {
-		duration: 0.5,
-		scrollTo: e.target.getAttribute('href')
-	});
+//ARROW SCROLL
+document.getElementById("scroll").addEventListener("click", function (e) {
+  e.preventDefault();
+  gsap.to(window, {
+    duration: 0.5,
+    scrollTo: e.target.getAttribute("href"),
+  });
 });
 
-//ARROW BOUNCE 
-gsap.from('.scrollArrow i', {
-	y: 40,
-	duration: 0.7,
-	yoyo: true,
-	repeat: -1,
-	ease: Bounce.Out
-})
-
-
-
-
-
+//ARROW BOUNCE
+gsap.from(".scrollArrow i", {
+  y: 40,
+  duration: 0.7,
+  yoyo: true,
+  repeat: -1,
+  ease: Bounce.Out,
+});
